cfebaef561be3f96969e5777e7a1109706cb79b0
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-08-08 00:24:04 +0530

    redux actions

---
+++ src/Components/CakeCointainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeCointainer() {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeCointainer
