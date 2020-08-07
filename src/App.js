89436d63c2beb54402c82343eb251025e6f8ad88
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-08-08 00:35:08 +0530

    redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import './App.css';
 import CakeCointainer from './Components/CakeCointainer';
 
 function App() {
   return (
-    <div className="App">
-     <CakeCointainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeCointainer />
+      </div>
+    </Provider>
   );
 }
 
